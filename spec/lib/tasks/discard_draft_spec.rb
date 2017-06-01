9cd52f2ed4948d86cf35983ff18222bf6ad525b9
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-06-01 15:02:42 +0100

    Add rake task to discard drafts for a content item

--- spec/lib/tasks/discard_draft_spec.rb
+++ spec/lib/tasks/discard_draft_spec.rb
@@ -2,12 +2,9 @@ require "rails_helper"
 
 RSpec.describe "Discard draft rake task" do
   it "runs the process to discard a draft" do
-    expect(Commands::V2::DiscardDraft).to receive(:call).with({ content_id: "content_id" })
+    payload = { content_id: "content_id" }
+    expect(Commands::V2::DiscardDraft).to receive(:call).with(payload)
 
     Rake::Task['discard_draft'].invoke("content_id")
-  end
-
-  it "raises an error if a content id is not provided" do
-    expect { Rake::Task['discard_draft'].invoke }.to raise_error('Missing parameter: content_id')
   end
 end
